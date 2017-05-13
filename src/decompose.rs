304fb0022d144f4060d245bb197ae39cf0febff4
Jamie Blondin <jblondin@gmail.com>
2017-05-13 15:33:08 -0400

    Updated SVD to use Compose trait, added tests
    Added narrow / wide matrix tests, and some utility function to help
    generate the singular value matrix.

--- src/decompose.rs
+++ src/decompose.rs
@@ -177,10 +177,39 @@ impl QRDecompose for Matrix {
 
 #[derive(Debug, Clone)]
 pub struct SVD<T, U> {
-    pub u: T,
-    pub sigma: U,
-    pub v: T,
+    u: T,
+    sigma: U,
+    v: T,
 }
+impl SVD<Matrix, Vec<f64>> {
+    pub fn u(&self) -> &Matrix {
+        &self.u
+    }
+    pub fn sigmavec(&self) -> &Vec<f64> {
+        &self.sigma
+    }
+    pub fn sigmamat(&self) -> Matrix {
+        let (m, n) = (self.u.ncols(), self.v.nrows());
+        let mut sigma = Matrix::zeros(m, n);
+        for i in 0..m {
+            for j in 0..n {
+                if i == j {
+                    sigma.set(i, j, self.sigma[i]).unwrap();
+                }
+            }
+        }
+        sigma
+    }
+    pub fn v(&self) -> &Matrix {
+        &self.v
+    }
+}
+impl Compose<Matrix> for SVD<Matrix, Vec<f64>> {
+    fn compose(&self) -> Matrix {
+        &self.u * self.sigmamat() * &self.v.t()
+    }
+}
+
 pub trait SingularValueDecompose: Sized {
     type SingularValueStore;
 
@@ -330,22 +359,63 @@ mod tests {
         qr_test_driver(8, 6);
     }
 
-    #[test]
-    fn test_svd() {
+    fn svd_test_driver(u: Matrix, sigma_diag: Vec<f64>, v: Matrix) {
+        let (m, n) = (u.nrows(), v.nrows());
+        assert_eq!(u.nrows(), u.ncols());
+        assert_eq!(v.nrows(), v.ncols());
+        assert_eq!(sigma_diag.len(), min(m, n));
+
+        // make sure input U is unitary
+        assert_fpvec_eq!(Matrix::eye(m), &u * &u.t(), 1e-5);
+        assert_fpvec_eq!(Matrix::eye(m), &u.t() * &u, 1e-5);
+
+        // make sure input V is unitary
+        assert_fpvec_eq!(Matrix::eye(n), &v * &v.t(), 1e-5);
+        assert_fpvec_eq!(Matrix::eye(n), &v.t() * &v, 1e-5);
+
+        let mut sigma = Matrix::zeros(m, n);
+        for i in 0..m {
+            for j in 0..n {
+                if i == j {
+                    sigma.set(i, j, sigma_diag[i]).unwrap();
+                }
+            }
+        }
+
+        // compose the matrix
+        let mat = &u * &sigma * &v.t();
+        println!("mat:\n{}", mat);
+
+        // decompose it back to get the singular values
+        let svd = mat.svd();
+        println!("{:#?}", svd);
+
+        // make sure singular values are identical
+        assert_fpvec_eq!(svd.sigma, sigma_diag);
+        println!("orig sigma: {:?}\ncomputed sigma: {:?}", sigma_diag, svd.sigma);
+
+        // make sure return U and V matrices are unitary
+        assert_fpvec_eq!(Matrix::eye(m), &svd.u * &svd.u.t(), 1e-5);
+        assert_fpvec_eq!(Matrix::eye(m), &svd.u.t() * &svd.u, 1e-5);
+        assert_fpvec_eq!(Matrix::eye(n), &svd.v * &svd.v.t(), 1e-5);
+        assert_fpvec_eq!(Matrix::eye(n), &svd.v.t() * &svd.v, 1e-5);
+
+        // make sure the SVD recomposes properly
+        let mat_composed = svd.compose();
+        println!("diff: {}", &mat - &mat_composed);
+        assert_fpvec_eq!(mat, mat_composed);
+
+        // also try to compose manually
+        let mat_composedmanual = &svd.u * &svd.sigmamat() * &svd.v.t();
+        println!("diff: {}", &mat - &mat_composedmanual);
+        assert_fpvec_eq!(mat, mat_composedmanual);
+    }
+
+    fn generate_singular_values(m: usize) -> Vec<f64> {
         let mut rng = rand::thread_rng();
 
-        let u = mat![-0.498315,  0.128598, -0.301661, -0.427864,  0.155238,  0.661044;
-                     -0.091854, -0.497697,  0.140542, -0.738935,  0.099853, -0.410016;
-                      0.285827,  0.161231, -0.737995, -0.048587,  0.503824, -0.302443;
-                      0.506196,  0.116019,  0.492270, -0.158451,  0.587474,  0.343138;
-                      0.636511, -0.152503, -0.279146, -0.285588, -0.552441,  0.326989;
-                     -0.012949, -0.820395, -0.156077,  0.402342,  0.248546,  0.280661];
-        // make sure input U is unitary
-        assert_fpvec_eq!(Matrix::eye(6), &u * &u.t(), 1e-5);
-        assert_fpvec_eq!(Matrix::eye(6), &u.t() * &u, 1e-5);
-
         let mut sigma_diag: Vec<f64> = vec![];
-        for _ in 0..6 {
+        for _ in 0..m {
             sigma_diag.push(rng.gen());
         }
         (&mut sigma_diag).sort_by(|&x, &y| {
@@ -353,8 +423,18 @@ mod tests {
             else if x < y { Ordering::Greater }
             else { Ordering::Less }
         });
-        let sigma = Matrix::diag(&sigma_diag);
-        println!("{:#?}", sigma);
+        sigma_diag
+    }
+
+    #[test]
+    fn test_svd_square() {
+
+        let u = mat![-0.498315,  0.128598, -0.301661, -0.427864,  0.155238,  0.661044;
+                     -0.091854, -0.497697,  0.140542, -0.738935,  0.099853, -0.410016;
+                      0.285827,  0.161231, -0.737995, -0.048587,  0.503824, -0.302443;
+                      0.506196,  0.116019,  0.492270, -0.158451,  0.587474,  0.343138;
+                      0.636511, -0.152503, -0.279146, -0.285588, -0.552441,  0.326989;
+                     -0.012949, -0.820395, -0.156077,  0.402342,  0.248546,  0.280661];
 
         let v = mat![-0.187616,  0.280013,  0.193183,  0.828045, -0.337414,  0.222637;
                       0.429073, -0.416271, -0.064585,  0.358640, -0.194242, -0.687088;
@@ -362,31 +442,44 @@ mod tests {
                      -0.495611, -0.559239,  0.658957, -0.047464,  0.028993, -0.065597;
                      -0.153013, -0.251152, -0.292822, -0.275479, -0.848193,  0.180126;
                      -0.392472, -0.435827, -0.643656,  0.312992,  0.350907,  0.143626];
-        // make sure input V is unitary
-        assert_fpvec_eq!(Matrix::eye(6), &v * &v.t(), 1e-5);
-        assert_fpvec_eq!(Matrix::eye(6), &v.t() * &v, 1e-5);
 
-        // compose the matrix
-        let m = &u * &sigma * &v.t();
-        println!("m:\n{}", m);
-
-        // decompose it back to get the singular values
-        let svd = m.svd();
-        println!("{:#?}", svd);
-
-        // make sure singular values are identical
-        assert_fpvec_eq!(svd.sigma, sigma_diag);
-
-        // make sure return U and V matrices are unitary
-        assert_fpvec_eq!(Matrix::eye(6), &svd.u * &svd.u.t(), 1e-5);
-        assert_fpvec_eq!(Matrix::eye(6), &svd.u.t() * &svd.u, 1e-5);
-        assert_fpvec_eq!(Matrix::eye(6), &svd.v * &svd.v.t(), 1e-5);
-        assert_fpvec_eq!(Matrix::eye(6), &svd.v.t() * &svd.v, 1e-5);
-
-        // make sure the SVD recomposes properly
-        let m_composed = &svd.u * Matrix::diag(&svd.sigma) * &svd.v.t();
-
-        println!("diff: {}", &m - &m_composed);
-        assert_fpvec_eq!(m, m_composed);
+        svd_test_driver(u, generate_singular_values(6), v);
     }
+
+    #[test]
+    fn test_svd_narrow() {
+
+        let u = mat![-0.498315,  0.128598, -0.301661, -0.427864,  0.155238,  0.661044;
+                     -0.091854, -0.497697,  0.140542, -0.738935,  0.099853, -0.410016;
+                      0.285827,  0.161231, -0.737995, -0.048587,  0.503824, -0.302443;
+                      0.506196,  0.116019,  0.492270, -0.158451,  0.587474,  0.343138;
+                      0.636511, -0.152503, -0.279146, -0.285588, -0.552441,  0.326989;
+                     -0.012949, -0.820395, -0.156077,  0.402342,  0.248546,  0.280661];
+
+        let v =  mat![-0.725341,  0.527853, -0.441127, -0.025669;
+                      -0.597937, -0.367570,  0.514698,  0.492392;
+                      -0.340529, -0.442671,  0.078282, -0.825805;
+                      -0.019763, -0.624745, -0.731003,  0.273747];
+
+        svd_test_driver(u, generate_singular_values(4), v);
+    }
+
+    #[test]
+    fn test_svd_wide() {
+
+        let u =  mat![-0.725341,  0.527853, -0.441127, -0.025669;
+                      -0.597937, -0.367570,  0.514698,  0.492392;
+                      -0.340529, -0.442671,  0.078282, -0.825805;
+                      -0.019763, -0.624745, -0.731003,  0.273747];
+
+        let v = mat![-0.498315,  0.128598, -0.301661, -0.427864,  0.155238,  0.661044;
+                     -0.091854, -0.497697,  0.140542, -0.738935,  0.099853, -0.410016;
+                      0.285827,  0.161231, -0.737995, -0.048587,  0.503824, -0.302443;
+                      0.506196,  0.116019,  0.492270, -0.158451,  0.587474,  0.343138;
+                      0.636511, -0.152503, -0.279146, -0.285588, -0.552441,  0.326989;
+                     -0.012949, -0.820395, -0.156077,  0.402342,  0.248546,  0.280661];
+
+        svd_test_driver(u, generate_singular_values(4), v);
+    }
+
 }
